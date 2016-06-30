018e416b5d39c237029446a578b853d7b83d6d03
AdrianBrown1 <adrian.angel.brown@gmail.com>
2016-06-30 15:11:36 -0400

    fun with swift playgorund

--- Playgrounds.playground/Contents.swift
+++ Playgrounds.playground/Contents.swift
@@ -1,2 +1,15 @@
 let str = "Hello, world"
-str = "Goodbye, world"
+//str = "Goodbye, world"
+
+
+let firstName = "Adrian"
+let lastName = "Brown"
+
+var fullName = "\(firstName) Angel \(lastName)"
+print(fullName)
+
+var age = 22
+
+let sentence = "Next year I will be \(age + 1) years old"
+print(sentence)
+
